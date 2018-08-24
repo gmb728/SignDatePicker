c075979fbe128425cecffd1500568d1b73384835
Chang Sophia <you@example.com>
2018-08-24 13:18:53 +0800

    initial

--- MondayDatePicker/ViewController.swift
+++ MondayDatePicker/ViewController.swift
@@ -7,8 +7,64 @@
 //
 
 import UIKit
+var timer: Timer?
+var goal: Date!
+let images =  ["1","2","3","4","5","6","7"]
+var i = 0
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myDatePickerValue: UIDatePicker!
+    
+    @IBOutlet weak var weekImageView: UIImageView!
+    @IBOutlet weak var weekTextField: UITextField!
+    @IBOutlet weak var goalTextField: UITextField!
+    @IBOutlet weak var mySegmentControl: UISegmentedControl!
+    
+    
+    @IBAction func myDatePickerValue(_ sender: UIDatePicker) {
+        goal = sender.date
+         goalTextField.isHidden = true
+        let today = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE"
+        let weekDay = formatter.string(from: today)
+        weekTextField.text = ("\(weekDay)")
+        goalTextField.text = formatter.string(from: myDatePickerValue.date)
+        i = i + 1
+        if i >= 7 {
+            i = 0
+         
+        }
+    }
+  
+    
+    @IBAction func mySegmentSelect(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0 && goalTextField.text == "Monday"){
+            self.weekImageView.image = UIImage(named: "1")
+            goalTextField.isHidden = false
+        }else if (sender.selectedSegmentIndex == 1 && goalTextField.text == "Tuesday"){
+            self.weekImageView.image = UIImage(named: "2")
+            goalTextField.isHidden = false
+        }else if (sender.selectedSegmentIndex == 2 && goalTextField.text == "Wednesday"){
+            self.weekImageView.image = UIImage(named: "3")
+            goalTextField.isHidden = false
+       }else if (sender.selectedSegmentIndex == 3 && goalTextField.text == "Thursday"){
+            self.weekImageView.image = UIImage(named: "4")
+            goalTextField.isHidden = false
+       }else if (sender.selectedSegmentIndex == 4 && goalTextField.text == "Friday"){
+            self.weekImageView.image = UIImage(named: "5")
+            goalTextField.isHidden = false
+       }else if (sender.selectedSegmentIndex == 5 && goalTextField.text == "Saturday"){
+            self.weekImageView.image = UIImage(named: "6")
+            goalTextField.isHidden = false
+       }else if (sender.selectedSegmentIndex == 6 && goalTextField.text == "Sunday"){
+            self.weekImageView.image = UIImage(named: "7")
+            goalTextField.isHidden = false
+        } else {self.weekImageView.image = UIImage(named: "8")
+            goalTextField.isHidden = false
+            
+        }
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +76,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+        override func viewDidDisappear(_ animated: Bool) {
+            if timer != nil {
+                timer?.invalidate()
+            }
+}
 }
 
